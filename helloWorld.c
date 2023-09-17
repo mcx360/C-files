00895c115a42f6cae16ae8db913a2aaf4c9eda0b
Michal Kornacki <mkornacki888@gmail.com>
2023-09-17 19:00:38 +0100

    first c program

---
+++ helloWorld.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+
+    //all basic escape sequences are the same as in java
+
+    printf("Hello World!\n");
+    int age =18;
+    float qca = 2.8;
+    char grade = 'C';
+    char name[] = "Michal";
+
+    //%s is the format specifier for an array of characters
+    printf("Hello %s\n", name);
+
+    //%d is format speccifier for type int, and age is the variable being used
+    printf("You are %d years old",age);
+
+    //%c is format speccifier for type char
+    printf("Your average grade is %c\n", grade);
+
+    //%f is format specifier for type float
+    printf("Your qca is %f\n", qca);  
+    
+    return 0;
+}
